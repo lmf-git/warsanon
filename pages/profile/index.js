a9c0ab1738816145055e44aba5639ab894e32314
L M Fielding <contact@lmfielding.com>
2022-09-21 11:26:39 +0100

    Whoops

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,12 +1,11 @@
+import { useContext, useState } from "react";
 import { getAuth, updateEmail, updateProfile } from "firebase/auth";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
 import Layout from '@components/Layout/Layout';
 
-import { useContext, useState } from "react";
-
 import Context from "@components/Context";
-import styles from '@components/Complete/Complete.module.css';
+import styles from '@components/Profile/Complete/Complete.module.css';
 
 export default function Profile() {
   const { auth } = useContext(Context);
